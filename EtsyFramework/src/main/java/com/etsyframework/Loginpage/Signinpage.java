d6cbbf053a1591b9301293dab70210873735ee5d
UjjwalaChaudhari <60313254+UjjwalaChaudhari@users.noreply.github.com>
2020-04-08 21:36:11 +0530

    Add files via upload

---
+++ EtsyFramework/src/main/java/com/etsyframework/Loginpage/Signinpage.java
@@ -0,0 +1,49 @@
+package com.etsyframework.Loginpage;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class Signinpage 
+{
+
+@FindBy(xpath="//div[@class='wt-flex-shrink-xs-0']")	
+private WebElement clicksignin;
+
+@FindBy(xpath="//*[@id='join_neu_email_field']")
+private WebElement emailorphone;
+
+@FindBy(xpath="//*[@id='join_neu_password_field']")
+private WebElement passwordTb;
+
+@FindBy(xpath="//button[@name='submit_attempt']")
+private WebElement loginclick;
+
+
+public void clicksignin() 
+{
+	clicksignin.click();
+}
+
+public void enteremailorphone(String value) 
+{
+	emailorphone.sendKeys(value);
+}
+public void clearemailorphoneTb() 
+{
+	emailorphone.clear();
+}
+public void enterpasswordTb(String value) 
+{
+	passwordTb.sendKeys(value);
+}
+public void clearpassword() 
+{
+passwordTb.clear();
+}
+
+public void Login()
+{
+loginclick.click();
+}
+
+}
